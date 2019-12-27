870ee2ad7b2c20f37464c8bca26ca510c12cb9bc
Aaron <aaronjameshorne57@gmail.com>
2019-12-27 11:47:55 -0500

    creating subnets with tags

--- terraform_project/vpc_and_ec2/infrastructure/variables.tf
+++ terraform_project/vpc_and_ec2/infrastructure/variables.tf
@@ -7,3 +7,11 @@ variable "vpc_cidr" {
   default = "10.0.0.0/16"
   description = "VPC cidr_block"
 }
+
+variable "public_vpc_subnet_1_cidr" {
+  description = "Public Subnet 1 CIDR"
+}
+
+variable "public_vpc_subnet_2_cidr" {
+  description = "Public Subnet 2 CIDR"
+}
--- terraform_project/vpc_and_ec2/infrastructure/vpc.tf
+++ terraform_project/vpc_and_ec2/infrastructure/vpc.tf
@@ -15,3 +15,22 @@ resource "aws_vpc" "production-vpc" {
   }
 }
 
+resource "aws_subnet" "public-subnet-1" {
+  cidr_block = "${var.public_vpc_subnet_1_cidr}"
+  vpc_id     = "${aws_vpc.production-vpc.id}"
+  availability_zone = "us-west-1"
+
+  tags {
+      name = "public-subnet-1" 
+  }
+}
+
+resource "aws_subnet" "public-subnet-2" {
+  cidr_block = "${var.public_vpc_subnet_2_cidr}"
+  vpc_id     = "${aws_vpc.production-vpc.id}"
+  availability_zone = "us-west-1"
+
+  tags {
+      name = "public-subnet-2" 
+  }
+}
