5e5d241426ccec937e081d971a360b2fe310db8e
Aaron <aaronjameshorne57@gmail.com>
2019-12-30 11:43:08 -0500

    eip and nat gateway

--- terraform_project/vpc_and_ec2/infrastructure/vpc.tf
+++ terraform_project/vpc_and_ec2/infrastructure/vpc.tf
@@ -159,4 +159,5 @@ resource "aws_internet_gateway" "production-igw" {
 resource "aws_route" "public-internet-gw-route" {
   route_table_id = "${aws_route_table.public-route_table.id}"
   gateway_id = "${aws_internet_gatewat.production-igw.id}"
+  destination_cidr_block = "0.0.0.0/0"
 }
