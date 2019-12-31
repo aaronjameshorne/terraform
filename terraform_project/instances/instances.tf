0839ca63311798e802f637e6630749037a821471
Aaron <aaronjameshorne57@gmail.com>
2019-12-31 11:56:22 -0500

    adding ec2 sg

--- terraform_project/instances/instances.tf
+++ terraform_project/instances/instances.tf
@@ -49,4 +49,18 @@ resource "aws_security_group" "ec2_private_security_group" {
   name = "EC2-Private-SG"
   description = "Only allow public security group resouces to access these instances"
   vpc_id = "${data.terraform_remote_state.network_configuration.vpc_id}"
+
+  ingress {
+      from_port = 0
+      protocol = "-1"
+      to_port = 0
+      cidr_blocks = ["${aws_security_group.ec2_public_security_group.id}"]
+  }
+
+    ingress {
+        from_port = 80
+        protocol = "TCP"
+        to_port = 80
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
