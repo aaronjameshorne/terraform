73af339d661539ab416989748d186dea58eae97b
Aaron <aaronjameshorne57@gmail.com>
2019-12-31 11:28:30 -0500

    adding instances

--- terraform_project/instances/instances.tf
+++ terraform_project/instances/instances.tf
@@ -15,3 +15,30 @@ data "terraform_remote_state" "network_configuration" {
         region = "${var.region}"
     }
 }
+
+resource "aws_security_group" "ec2_public_security_group" {
+  name = "EC2-Public-SG"
+  description = "Internet reaching access for EC2 instances "
+  vpc_id = "${data.terraform_remote_state.network_configuration.vpc_id}"
+}
+
+ingress {
+    from_port = 80
+    protocol = "TCP"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+ingress {
+    from_port = 22
+    protocol = "TCP"
+    to_port = 22
+    cidr_blocks = ["45.73.149.202"]
+}
+
+egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+}
