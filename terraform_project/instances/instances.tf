190f9502534193f87c78efe5e3f7f9d74025b8b9
Aaron <aaronjameshorne57@gmail.com>
2019-12-31 11:19:51 -0500

    adding instances

--- terraform_project/instances/instances.tf
+++ terraform_project/instances/instances.tf
@@ -10,6 +10,8 @@ terraform {
 data "terraform_remote_state" "network_configuration" {
     backend = "s3"
     config {
-        bucket = ""
+        bucket = "${var.remote_state_bucket}"
+        key = "${var.remote_state_key}"
+        region = "${var.region}"
     }
 }
--- terraform_project/instances/variables.tf
+++ terraform_project/instances/variables.tf
@@ -5,6 +5,11 @@ variable "region" {
 
 
 variable "remote_state_bucket" {
-    description "Bucket name for layer 1 remote state"
+    description = "Bucket name for layer 1 remote state"
+  
+}
+
+variable "remote_state_key" {
+    description = "Key name for layer 1 remote state"
   
 }
