ebf04110c6eafb393f10295e852302aa2370e7a1
Aaron Horne <aaronjameshorne57@gmail.com>
2020-01-04 23:51:54 -0500

    removing tags since there errors in .012. No az needed and removing

--- terraform_project/vpc_and_ec2/infrastructure/vpc.tf
+++ terraform_project/vpc_and_ec2/infrastructure/vpc.tf
@@ -7,86 +7,45 @@ provider "aws" {
 resource "aws_vpc" "production-vpc" {
   cidr_block            = "${var.vpc_cidr}"
   enable_dns_hostnames  = true
-
-  tags {
-    Name = "Production-VPC"
-  }
 }
 
 resource "aws_subnet" "public-subnet-1" {
   cidr_block        = "${var.public_subnet_1_cidr}"
   vpc_id            = "${aws_vpc.production-vpc.id}"
-  availability_zone = "us-west-1a"
-
-  tags {
-    Name = "Public-Subnet-1"
-  }
 }
 
 resource "aws_subnet" "public-subnet-2" {
   cidr_block        = "${var.public_subnet_2_cidr}"
   vpc_id            = "${aws_vpc.production-vpc.id}"
-  availability_zone = "us-west-1b"
-
-  tags {
-    Name = "Public-Subnet-2"
-  }
 }
 
 resource "aws_subnet" "public-subnet-3" {
   cidr_block        = "${var.public_subnet_3_cidr}"
   vpc_id            = "${aws_vpc.production-vpc.id}"
-  availability_zone = "us-west-1c"
-
-  tags {
-    Name = "Public-Subnet-3"
-  }
 }
 
 resource "aws_subnet" "private-subnet-1" {
   cidr_block        = "${var.private_subnet_1_cidr}"
   vpc_id            = "${aws_vpc.production-vpc.id}"
-  availability_zone = "us-west-1a"
-
-  tags {
-    Name = "Private-Subnet-1"
-  }
 }
 
 resource "aws_subnet" "private-subnet-2" {
   cidr_block        = "${var.private_subnet_2_cidr}"
   vpc_id            = "${aws_vpc.production-vpc.id}"
-  availability_zone = "us-west-1b"
-
-  tags {
-    Name = "Private-Subnet-2"
-  }
 }
 
 resource "aws_subnet" "private-subnet-3" {
   cidr_block        = "${var.private_subnet_3_cidr}"
   vpc_id            = "${aws_vpc.production-vpc.id}"
-  availability_zone = "us-west-1c"
-
-  tags {
-    Name = "Private-Subnet-3"
-  }
 }
 
 resource "aws_route_table" "public-route-table" {
   vpc_id = "${aws_vpc.production-vpc.id}"
 
-  tags {
-    Name = "Public-Route-Table"
-  }
 }
 
 resource "aws_route_table" "private-route-table" {
   vpc_id = "${aws_vpc.production-vpc.id}"
-
-  tags {
-    Name = "Private-Route-Table"
-  }
 }
 
 resource "aws_route_table_association" "public-subnet-1-association" {
@@ -122,19 +81,12 @@ resource "aws_route_table_association" "private-subnet-3-association" {
 resource "aws_eip" "elastic-ip-for-nat-gw" {
   vpc                       = true
   associate_with_private_ip = "10.0.0.5"
-
-  tags {
-    Name = "Production-EIP"
-  }
 }
 
 resource "aws_nat_gateway" "nat-gw" {
   allocation_id = "${aws_eip.elastic-ip-for-nat-gw.id}"
   subnet_id     = "${aws_subnet.public-subnet-1.id}"
 
-  tags {
-    Name = "Production-NAT-GW"
-  }
 
   depends_on = ["aws_eip.elastic-ip-for-nat-gw"]
 }
@@ -147,10 +99,6 @@ resource "aws_route" "nat-gw-route" {
 
 resource "aws_internet_gateway" "production-igw" {
   vpc_id = "${aws_vpc.production-vpc.id}"
-
-  tags {
-    Name = "Production-IGW"
-  }
 }
 
 resource "aws_route" "public-internet-gw-route" {
