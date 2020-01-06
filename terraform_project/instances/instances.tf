a49b77cf70250b7860f6c68186dd2b1fe3daa1d2
Aaron Horne <aaronjameshorne57@gmail.com>
2020-01-06 15:29:33 -0500

    public ec2 addition

--- terraform_project/instances/instances.tf
+++ terraform_project/instances/instances.tf
@@ -173,4 +173,14 @@ resource "aws_launch_configuration" "ec2_public_launch_configuration" {
   associate_public_ip_address = true
   iam_instance_profile = "${aws_iam_instance_profile.ec2_instance_profile.name}"
   security_groups = ["${aws_security_group.ec2_public_security_group.id}"]
+
+  user_data = <<EOF
+  #!/bin/bash
+  yum update -y
+  yum install httpd24 -y
+  service httpd start
+  chkconfig httpd on
+  export INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+  echo  "<html><body><h1>Hello from Production Backend at instance<b>"$INSTANCE_ID"</b></h1></body?</html> > /var/www/html/index.html
+  EOF
 }
