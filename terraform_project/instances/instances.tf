d76bf6b01176090ab80ab176cb1f88ccf50e9d47
Aaron Horne <aaronjameshorne57@gmail.com>
2020-01-06 14:56:06 -0500

    adding ec2 meta data

--- terraform_project/instances/instances.tf
+++ terraform_project/instances/instances.tf
@@ -152,5 +152,16 @@ resource "aws_launch_configuration" "ec2_private_launch_configuration" {
   instance_type = "${var.ec2_instance_type}"
   key_name = "${var.key_pair_name}"
   associate_public_ip_address = false
+  iam_instance_profile = "${aws_iam_instance_profile.ec2_instance_profile.name}"
+  security_groups = ["${aws_security_group.ec2_private_security_group.id}"]
+
+  user_data = <<EOF
+  #!/bin/bash
+  yum update -y
+  yum install httpd24 -y
+  service httpd start
+  chkconfig httpd on
+  export INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+  EOF
 }
 
