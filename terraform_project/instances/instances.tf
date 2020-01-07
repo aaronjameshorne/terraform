9bc048f60541f5ba5ad1714a2f788a77ac7401a3
Aaron Horne <aaronjameshorne57@gmail.com>
2020-01-07 18:17:17 -0500

    adding autoscaling policy

--- terraform_project/instances/instances.tf
+++ terraform_project/instances/instances.tf
@@ -268,3 +268,10 @@ resource "aws_autoscaling_group" "ec2_public_autoscaling_group" {
 
 
 }
+
+resource "aws_autoscaling_policy" "webapp_production_scaling_policy" {
+  autoscaling_group_name = "${aws_autoscaling_group.ec2_public_autoscaling_group.name}"
+  name = "production-webapp-autoscaling-policy"
+  policy_type = "TargetTrackingPolicy"
+  min_adjustment_magnitude = 1
+}
