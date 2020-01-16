c4ee94889f51e6cbd079710a4976de286c5ee565
Aaron Horne <aaronjameshorne57@gmail.com>
2020-01-16 18:36:45 -0500

    adding provisioner and key pair

--- appointments/script.sh
+++ appointments/script.sh
@@ -6,8 +6,8 @@ until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
 done
 
 # install nginx
-apt-get update
-apt-get -y install nginx
+yum update
+yum -y install htop
 
 # make sure nginx is started
-service nginx start
+
