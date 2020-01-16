7dd406d678b34087ec35cd60fa461b1d34ad06ef
Aaron Horne <aaronjameshorne57@gmail.com>
2020-01-16 18:53:12 -0500

    adding provisioner and key pair

--- appointments/vars.tf
+++ appointments/vars.tf
@@ -1,22 +1,23 @@
+
 variable "AWS_REGION" {
   default = "us-west-1"
 }
 
 variable "AMIS" {
-  type = "map"
+  type = map(string)
   default = {
-      us-west-1 = "ami-03caa3f860895f82e"
+    us-west-1 = "ami-03caa3f860895f82e"
   }
 }
 
-variable "instance_username" {
-  default = "ec2-user"
-}
-
-variable "path_to_private_key" {
+variable "PATH_TO_PRIVATE_KEY" {
   default = "mykey"
 }
 
 variable "PATH_TO_PUBLIC_KEY" {
   default = "mykey.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
 }
