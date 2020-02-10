c0d709501e7701b9371a6b711c587c56950fc915
Aaron <aaronjameshorne57@gmail.com>
2020-02-10 07:19:07 -0500

    adding external ip to sg for access chewy network

--- remote-instance-template/instance.tf
+++ remote-instance-template/instance.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "allow_ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["73.139.34.225/32"]
+    cidr_blocks = ["73.139.34.225/32","45.73.149.202/32"]
   }
 
   egress {
