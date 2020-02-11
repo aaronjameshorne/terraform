1059dfaf863f6f679f57f7f0909736c16f4873c9
Aaron <aaronjameshorne57@gmail.com>
2020-02-10 22:08:23 -0500

    adding terrafor repo to ami

--- remote-instance-template/instance.tf
+++ remote-instance-template/instance.tf
@@ -54,6 +54,7 @@ sudo pip install -U pip
 sudo pip install -U virtualenv
 cd /home/ec2-user/
 git clone https://github.com/aaronjameshorne/python.git
+git clone https://github.com/aaronjameshorne/terraform.git
 sudo yum install epel-release
 sudo yum -y update
 curl -L https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh | bash
