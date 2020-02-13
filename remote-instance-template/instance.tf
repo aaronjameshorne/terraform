78289971b86c1bc80bac2894259db11479121d38
Aaron <aaronjameshorne57@gmail.com>
2020-02-13 14:34:53 -0500

    chaning order of python3 deployment

--- remote-instance-template/instance.tf
+++ remote-instance-template/instance.tf
@@ -67,7 +67,7 @@ EOF
 
   }
   provisioner "local-exec" {
-    command = "echo ${aws_instance.example.public_ip} > public_ips.txt"
+    command = "echo ssh ec2-user@${aws_instance.example.public_ip} > public_ips.txt"
   }
 }
 
