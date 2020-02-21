a71ea3c19ebbe17ebf9a232184e17dbb6701e594
Aaron <aaronjameshorne57@gmail.com>
2020-02-21 10:53:31 -0500

    adding user data as a var

--- remote-instance-template/instance.tf
+++ remote-instance-template/instance.tf
@@ -38,32 +38,11 @@ resource "aws_key_pair" "mykey" {
 }
 
 resource "aws_instance" "example" {
-  ami                   = "${data.aws_ami.amz2_linux.id}"
+  ami                    = "${data.aws_ami.amz2_linux.id}"
   instance_type          = "${var.t2-micro-size}"
   vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
   key_name               = "${aws_key_pair.mykey.key_name}"
-  user_data = <<EOF
-#!/bin/bash
-DD_AGENT_MAJOR_VERSION=7 DD_API_KEY=8b47966137e9f64b6005e591020698e8 bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
-sudo yum -y update
-sudo yum install -y htop
-sudo yum install -y vim
-sudo yum install -y git
-sudo yum install -y sshpass
-yum -y install python-pip
-sudo amazon-linux-extras install -y epel
-sudo yum install -y ansible
-sudo yum install -y python34 python-pip
-sudo pip install -U pip
-sudo pip install -U virtualenv
-cd /home/ec2-user/
-git clone https://github.com/aaronjameshorne/python.git
-git clone https://github.com/aaronjameshorne/terraform.git
-sudo yum install epel-release
-sudo yum -y update
-curl -L https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh | bash
-sudo yum install -y python34 python34-pip
-EOF
+  user_data              = "${var.package_install}"
 
   tags = {
     datadog      = "yes"
